963b3140d4accdc3a8a74f67acdd2128db23ce98
ArthusDias8 <arthusdias10@gmail.com>
2023-04-19 17:01:59 -0300

    Aula de funções

---
+++ aulaFunções/ex1.js
@@ -0,0 +1,20 @@
+
+//function soma(){
+//    let n1 = 2
+//    let n2 = 10
+//    let resultado = n1 + n2
+//    console.log(`o resultado da soma é ${resultado}`)
+//}
+
+//for(let i=0;i<2;i++){
+//    soma()
+//}
+
+function mudarTexto(){
+    let d1 = document.getElementById('d1')
+    let d2 = document.getElementById('d2')
+    let d3 = document.getElementById('d3')
+    d1.innerHTML= 'CFB cursos'
+    d2.innerHTML= 'CFB cursos'
+    d3.innerHTML= 'CFB cursos'
+}
